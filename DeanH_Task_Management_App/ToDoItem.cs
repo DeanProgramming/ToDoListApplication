be119b302edf6156534731af1e9f8b748fbcefe7
DeanProgramming <110070686+DeanProgramming@users.noreply.github.com>
2024-02-11 19:13:21 +0000

    Initial push

---
+++ DeanH_Task_Management_App/ToDoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace DeanH_Task_Management_App
+{
+    public class ToDoItem
+    { 
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsDone { get; set; } = false;
+        public DateOnly DeadLine { get; set; }
+        public int Priority { get; set; }
+    }
+}
